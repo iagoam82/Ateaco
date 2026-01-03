5bfda43683f983eb04b245222e0d4460ef3f11c3
Iago Alonso Meijide <81101500+iagoam82@users.noreply.github.com>
2026-01-03 13:58:52 +0100

    Create use-user.tsx

---
+++ src/firebase/auth/use-user.tsx
@@ -0,0 +1,22 @@
+'use client';
+
+import { useEffect, useState } from 'react';
+import { onAuthStateChanged, User } from 'firebase/auth';
+import { useAuth } from '../provider';
+
+export const useUser = () => {
+  const auth = useAuth();
+  const [user, setUser] = useState<User | null>(null);
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    const unsubscribe = onAuthStateChanged(auth, (user) => {
+      setUser(user);
+      setIsLoading(false);
+    });
+
+    return () => unsubscribe();
+  }, [auth]);
+
+  return { user, isLoading };
+};
